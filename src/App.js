d03e8f1eb2a962b5cf9e2d8911aa4b51af741e40
Crix Rosales <crimiro@MacBook-Pro-de-Crix.local>
2019-07-20 23:42:18 -0500

    Starting course and added styled-components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ListOfCategories } from './components/ListOfCategories/index'
+import { ListOfPhotoCards } from './components/ListOfPhotoCard'
+import { GlobalStyle } from './GlobalStyles'
+
+export const App = () => (
+  <div>
+    <GlobalStyle />
+    <ListOfCategories />
+    <ListOfPhotoCards />
+  </div>
+)

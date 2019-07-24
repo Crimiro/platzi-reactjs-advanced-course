c9d394a3655aca14f0738c310cdaa413f765be0c
Crix Rosales <crimiro@MacBook-Pro-de-Crix.local>
2019-07-23 22:24:56 -0500

    Starting hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories/index'
 import { ListOfPhotoCards } from './components/ListOfPhotoCard'
-import { GlobalStyle } from './GlobalStyles'
+import { Logo } from './components/Logo'
+import { GlobalStyle } from './components/styles/GlobalStyles'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
